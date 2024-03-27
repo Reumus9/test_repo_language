241653723f6252641d99789184fdd737a113f375
Dmitrii <arbuzvkysniy@gmail.com>
2024-03-27 17:16:05 +0300

    реализация теста для проверки наличия предпочтительного языка

--- conftest.py
+++ conftest.py
@@ -1,1 +1,31 @@
 # написать тест для проверки наличия кнопки корзины с определенным текстом на различных языках
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="ru",
+                     help="Choose language")
+    
+
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': browser_name})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
+
+@pytest.fixture(scope="function")
+def language_name(request):
+    return request.config.getoption("language")
+
+
+
+
+
+
--- test_items.py
+++ test_items.py
@@ -1,1 +1,23 @@
-#реализуй
+import time 
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_language(browser, language_name):
+    browser.get(link)
+    time.sleep(30)
+    # проверяю наличие наличие кнопки на странице
+    try:
+        button = WebDriverWait(browser, 5).until(ec.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket"))) 
+    except TimeoutException:
+        print("кнопки корзины нет на странице")
+        assert False
+    if language_name=="fr": # если предпочтительный язык является французским, то проверяю соответствует ли текст на кнопке fr
+        button_text = button.text
+        assert button_text == "Ajouter au panier", "техт не совпадает"
+    elif language_name!="fr":
+        assert True # так как мы ранее проверили наличие кнопки, то в остальных случаях у нас True
+
